6b998454200123f333a2bb4b5696667f66966c08
Vincent Lejeune <vljn.ovi@gmail.com>
2018-06-25 20:00:21 +0200

    Parse img/font name .

--- loader.py
+++ loader.py
@@ -1,22 +1,22 @@
-import torch.utils.data
+#import torch.utils.data
 import os
+from bs4 import BeautifulSoup
+import xml
+import xml.etree.ElementTree as ET
 
+#current_path = os.path.dirname(os.path.abspath(__file__))
+current_path = "C:\\Users\\vlj\\Documents\\GitHub\\VisualFontRecognition\\fonts"
 
-class FontLoader(torch.utils.data.Dataset):
-  def __init__(self, transform = None):
-    self.transform = transform
-    for path, dir, files in os.walk("C:\\windows\\fonts"):
-      for file in files:
-        if file.endswith(".ttf"):
-            self.fonts.append(file) 
+def get_entries():
+  for path, dir, files in os.walk(current_path):
+    for file in files:
+      f = open(os.path.join(path, file), "r").read()
+      #f = "<html><body><table>" + f + "</table></body><html>"
+      soup = BeautifulSoup(f, 'html.parser')
+      for tr in soup.find_all('tr'):
+        tds = tr.find_all('td')
+        if len(tds) == 7 and tds[5].a != None:
+          yield tds[0].img['src'], tds[5].a.contents
 
-  def __len__(self):
-    return len(self.fonts) * self.size
-
-  def __getitem__(self, i):
-    idx = i % len(self.fonts)
-    word = self.texts[i]
-    img = make_img(word, self.fonts[idx])
-    if self.transform:
-      img = self.transform(img)
-    return img, idx
+for t in get_entries():
+  print(t)
